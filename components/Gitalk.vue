f79293892b1dfb8e9d3e077801e4b4f8edbefe3d
zhangyuheng(vczyh) <vczyh@foxmail.com>
2020-04-13 02:00:23 +0800

    'undefined'

--- components/Gitalk.vue
+++ components/Gitalk.vue
@@ -12,26 +12,26 @@ import md5 from "blueimp-md5";
 export default {
   data() {
     return {
-    //   key: 0,
-    //   clientID: "",
-    //   clientSecret: "",
-    //   repo: "",
-    //   owner: "",
-    //   admin: [],
-    //   id: md5(location.pathname), // Ensure uniqueness and length less than 50
-    //   distractionFreeMode: false // Facebook-like distraction free mode
+      //   key: 0,
+      //   clientID: "",
+      //   clientSecret: "",
+      //   repo: "",
+      //   owner: "",
+      //   admin: [],
+      //   id: md5(location.pathname), // Ensure uniqueness and length less than 50
+      //   distractionFreeMode: false // Facebook-like distraction free mode
     };
   },
 
   methods: {
     init() {
       const { gitalk } = this.$site.themeConfig;
-      if(!gitalk) return 
+      if (!gitalk) return;
       gitalk.admin = [gitalk.owner];
       gitalk.id = md5(location.pathname);
       gitalk.distractionFreeMode = false;
-      const ele = document.getElementById('gitalk-container')
-      ele.innerHTML= ' '
+      const ele = document.getElementById("gitalk-container");
+      ele.innerHTML = " ";
       new Gitalk(gitalk).render("gitalk-container");
       // console.log(location.pathname)
       // console.log(gitalk)
@@ -39,12 +39,12 @@ export default {
   },
 
   mounted() {
-    this.init()
+    this.init();
   },
 
   watch: {
-    $route(to, from) {
-      this.init()
+    "$route.path": function(to, from) {
+      this.init();
     }
   }
 };
