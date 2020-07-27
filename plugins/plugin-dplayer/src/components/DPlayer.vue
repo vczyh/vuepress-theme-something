a28bea70cea16a54e46f6c0d94e834e00f843764
vczyh <zhangeek@qq.com>
2020-07-28 00:13:41 +0800

    添加dplayer插件

---
+++ plugins/plugin-dplayer/src/components/DPlayer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="dplayer" />
+</template>
+
+<script>
+import DPlayer from "dplayer";
+export default {
+  name: "DPlayer",
+  props: {
+    options: Object,
+  },
+  methods: {
+    load() {
+      const dp = new DPlayer({
+        container: document.getElementById("dplayer"),
+        ...this.options,
+      });
+    },
+  },
+
+  mounted() {
+    this.load();
+  },
+};
+</script>
+
+<style>
+</style>
